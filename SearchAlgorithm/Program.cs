be312919fd37020afd01311394667276d8afa27f
adrianrezha29 <adrianrezha29@gmail.com>
2022-10-28 16:07:15 +0700

    add method array to be searched

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -10,5 +10,22 @@ namespace SearchAlgorithm
     {
         //Array to be searched
         int[] arr = new int[20];
+        //Number of elements in the array
+        int i;
+
+        public void input()
+        { 
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+
+            }
+        
+        }
     }
 }
