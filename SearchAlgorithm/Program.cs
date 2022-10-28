6b4d0c46b64f9c0f8e63f7f1b0dc0733035ddf43
adrianrezha29 <adrianrezha29@gmail.com>
2022-10-28 16:31:49 +0700

    add method console writeline untuk menampilkan found at position,not found in the array, and continue search

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -63,6 +63,14 @@ namespace SearchAlgorithm
                     mid = (lowerbound + upperbound) / 2;
                     ctr++;
                 }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + " Found at position " + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " not found in the array\n ");
+                Console.WriteLine("\nNumber of Comparasion : " + ctr);
+
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
             
                         
             }
